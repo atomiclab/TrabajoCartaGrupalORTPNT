210904f7ad07f7ed460b767fdb221c0dce019bad
atomiclab <ginotubaro@gmail.com>
2024-09-17 14:29:24 -0300

    Correccion en estilos (grillas), agregado un "mapeo" en el js y señalizada la tabla hecha con bstrap

--- script.js
+++ script.js
@@ -12,17 +12,25 @@ function validarPedido() {
 
   let numero = parseInt(npedido.value);
 
+  const productos = [
+    "Americano", "Macchiato", "Cafe con Leche", "Latte", "Cappuccino",
+    "Chocolate con menta", "Cafe con sirope", "Cafe con doble crema", "Moca Latte",
+    "Cafe con Crema de Matcha", "Cappuccino frio", "Chocolate con menta", "Te Rooibos",
+    "Te con cafe y leche", "Te de frutos rojos", "Te de naranja", "Cappuccino con limon"
+];
+
   if (nombre.value === "" || apellido.value === "" || npedido.value === "") {
     alert("Campos vacios");
   }
 
   if (!isNaN(numero) && numero >= 0 && numero <= 17) {
     alert("Pedido realizado con exito");
+    let producto = productos[numero - 1];
     msgdefault.style.display = "none";
     pedidosrealizados.innerHTML += `
     <tr>
         <td>Realizado por: ${nombre.value} ${apellido.value}. </td>
-        <td>El producto solicitado es: ${npedido.value}</td>
+        <td>El producto solicitado es: ${npedido.value}, ${producto}</td>
     </tr>
     <br/>`;
   } else {
